55966b663d621bf42c1393c00cc5754141bb7a5b
BSA <email@email.com>
2021-01-14 23:07:10 +0300

    Initial commit

---
+++ src/test/java/sbertech/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package sbertech;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple HelloWorld.
+ */
+public class HelloWorldTest 
+    extends TestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
